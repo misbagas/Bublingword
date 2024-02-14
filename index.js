ca77ee910188759101303668ccf7679ac38cf614
Misbah bagaskara purwanto <56241375+misbagas@users.noreply.github.com>
2024-02-14 16:31:51 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const divs = document.getElementsByTagName('div');
+for (let el of divs) {
+    el.addEventListener('click', function(){
+        alert('ELEMEN' + el.getAttribute('id').toUpperCase());
+    }, true);
+
+    
+}
